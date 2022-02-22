8b042ad24daf6455665c73fdcfdd1309a9d54319
Артём <artem.zeynetdinov@mail.ru>
2022-02-22 14:09:40 +0300

    Работа

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FitnessAssistant_ZeynetdinovArtuhov_2ISP11_17
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Authorization_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Registration_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            RegistrationWindow openWindow = new RegistrationWindow();
+            openWindow.Show();
+            this.Close();
+        }
+    }
+}
